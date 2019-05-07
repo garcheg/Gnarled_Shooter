fff32a1a2a070299741f29b391b979fbd5c62466
garcheg <garcheg2281337@gmail.com>
2019-05-07 22:39:53 +0300

    1

--- GnarledShooter/GnarledShooter/main.cpp
+++ GnarledShooter/GnarledShooter/main.cpp
@@ -1,4 +1,12 @@
 // ~~~~~~~~~ [ Includes ] ~~~~~~~~~ //
+#include <Windows.h>
+#include <iostream>
+#include <string>
+
+using namespace std;
 // ~~~~~~~~~ [ Vars ] ~~~~~~~~~ //
 // ~~~~~~~~~ [ Func's ] ~~~~~~~~~ //
 // ~~~~~~~~~ [ Code ] ~~~~~~~~~ //
+int main() {
+	return 0;
+}
