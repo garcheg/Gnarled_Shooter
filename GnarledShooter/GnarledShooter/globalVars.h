bb0fcaf418e0a4b3ebed87d77ae7af114b182f89
garcheg <garcheg2281337@gmail.com>
2019-05-07 22:46:22 +0300

    ds

--- GnarledShooter/GnarledShooter/globalVars.h
+++ GnarledShooter/GnarledShooter/globalVars.h
@@ -6,6 +6,16 @@
 
 using namespace std;
 
+
 struct globalVars {
-	bool hui = false;
+	struct {
+		CONST BOOL DEVELOPER_MODE = true; // Developer mode
+
+		CONST INT WINDOW_WIDTH = 1280; // Width window
+		CONST INT WINDOW_HEIGHT = 720; // Height window
+	} GLOBAL_SETTINGS;
+
+
+
+
 }; extern globalVars gVars;
